f0d343253355bb78a8012439725cef3116953410
96wais <awaisumar773@gmail.com>
2025-11-04 01:45:39 +0000

    Add files via upload

--- 03multiples/sketch.js
+++ 03multiples/sketch.js
@@ -1,7 +1,28 @@
+let theText = ["hello", "random", "words", "to", "display", "on", "the", "screen"];
+let word = theText[0];
+let i = 0;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(60, 60, 120);
+
+  // The text display
+  textSize(60);
+  text(word, random(350), height/2)
+
+  // Change the value of word 
+  word = theText[i];
+  i=i+1;
+  if(i>=theText.length){
+    i=0;
+  }
+  console.log(i);
+
 }
+
+
+
+
